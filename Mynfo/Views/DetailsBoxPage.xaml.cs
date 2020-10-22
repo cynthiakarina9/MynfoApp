7e937d209580253ea4aad7ba34f16d8a26698a13
Rodrigo Rodriguez Toachee <rrodriguez@atx.mx>
2020-10-21 23:49:22 -0500

    Cambios de boxes, detalles y eliminar relaciones - 21/10/20 - Rodrigo

--- Mynfo/Views/DetailsBoxPage.xaml.cs
+++ Mynfo/Views/DetailsBoxPage.xaml.cs
@@ -7,6 +7,8 @@ using System.Data.SqlClient;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using System.Text;
+using Xamarin.Forms.Markup;
+using Xamarin.Forms.Shapes;
 
 namespace Mynfo.Views
 {
@@ -27,6 +29,9 @@ namespace Mynfo.Views
             string queryLastBoxCreated = "SELECT TOP 1 * FROM dbo.Boxes where dbo.Boxes.UserId = " + UserID + " ORDER BY BoxId DESC";
             string queryUpdatesetDefault;
             string queryUpdateTakeOffDefault;
+            string queryGetPhones;
+            string queryGetEmails;
+            string queryGetSMProfiles;
             string cadenaConexion = @"data source=serverappmyinfonfc.database.windows.net;initial catalog=mynfo;user id=adminatxnfc;password=4dmiNFC*Atx2020;Connect Timeout=60";
             System.Text.StringBuilder sb;
             String BoxName = "";
@@ -65,6 +70,17 @@ namespace Mynfo.Views
             consultaDefault = "select * from dbo.Boxes where dbo.Boxes.BoxId = " + BoxId;
             queryUpdatesetDefault = "update dbo.Boxes set BoxDefault = 1 where dbo.Boxes.UserId =" + UserID + " and dbo.Boxes.BoxId =" + BoxId;
             queryUpdateTakeOffDefault = "update dbo.Boxes set BoxDefault = 0 where dbo.Boxes.UserId =" + UserID + " and dbo.Boxes.BoxDefault = 1 and dbo.Boxes.BoxId !=" + BoxId;
+            queryGetPhones = "select dbo.Boxes.BoxId, dbo.ProfilePhones.ProfilePhoneId, dbo.ProfilePhones.Name, " + 
+                             "dbo.ProfilePhones.Number from dbo.Box_ProfilePhone Join dbo.Boxes " +
+                             "on(dbo.Boxes.BoxId = dbo.Box_ProfilePhone.BoxId) "+
+                             "Join dbo.ProfilePhones on(dbo.ProfilePhones.ProfilePhoneId = dbo.Box_ProfilePhone.ProfilePhoneId) "+
+                             "where dbo.Boxes.BoxId = " + BoxId;
+            queryGetEmails = "select dbo.Boxes.BoxId, dbo.ProfileEmails.ProfileEmailId, dbo.ProfileEmails.Name, " +
+                              "dbo.ProfileEmails.Email from dbo.Box_ProfileEmail " +
+                              "Join dbo.Boxes on(dbo.Boxes.BoxId = dbo.Box_ProfileEmail.BoxId) " +
+                              "Join dbo.ProfileEmails on(dbo.ProfileEmails.ProfileEmailId = dbo.Box_ProfileEmail.ProfileEmailId) " +
+                              "where dbo.Boxes.BoxId = " + BoxId;
+            queryGetSMProfiles = "select * from dbo.ProfileSMs where dbo.ProfileSMs.UserId = " + UserID;
 
             //Consulta para obtener Box
             using (SqlConnection connection = new SqlConnection(cadenaConexion))
@@ -123,6 +139,111 @@ namespace Mynfo.Views
 
             BoxDefaultCheckBox.Children.Add(BxDefaultCheckBox);
 
+            //**********************************************************
+            //AGREGACIÓN DE PERFILES A BOX
+            //**********************************************************
+
+            //Consulta para obtener Teléfonos
+            using (SqlConnection connection = new SqlConnection(cadenaConexion))
+            {
+                sb = new System.Text.StringBuilder();
+                sb.Append(queryGetPhones);
+                string sql = sb.ToString();
+
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var phoneName = new Label();
+                            var phoneNumber = new Label();
+                            var deleteProfile = new Button();
+                            var Line = new BoxView();
+                            int PhoneId = (int)reader["ProfilePhoneId"];
+
+                            phoneName.Text = (string)reader["Name"];
+                            phoneName.FontSize = 15;
+
+                            phoneNumber.Text = (string)reader["Number"];
+                            phoneNumber.FontSize = 25;
+                            phoneNumber.HorizontalTextAlignment = TextAlignment.Center;
+
+                            deleteProfile.Text = "B";
+                            deleteProfile.TextColor = Color.Black;
+                            deleteProfile.FontSize = 10;
+                            deleteProfile.BackgroundColor = Color.FromHex("#f9a589");
+                            deleteProfile.CornerRadius = 15;
+                            deleteProfile.HeightRequest = 30;
+                            deleteProfile.WidthRequest = 30;
+                            deleteProfile.HorizontalOptions = LayoutOptions.End;
+                            deleteProfile.Clicked += new EventHandler((sender, e) => DeleteBoxPhone(sender, e, BoxId, PhoneId));
+
+                            Line.HeightRequest = 1;
+                            Line.Color = Color.FromHex("#FF5521");
+
+                            ProfilesList.Children.Add(phoneName);
+                            ProfilesList.Children.Add(phoneNumber);
+                            ProfilesList.Children.Add(deleteProfile);
+                            ProfilesList.Children.Add(Line);
+                        }
+                    }
+                    connection.Close();
+                }
+            }
+
+            //Consulta para obtener Emails
+            using (SqlConnection connection = new SqlConnection(cadenaConexion))
+            {
+                sb = new System.Text.StringBuilder();
+                sb.Append(queryGetEmails);
+                string sql = sb.ToString();
+
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var emailProfile = new Label();
+                            var emailAddress = new Label();
+                            var deleteProfile = new Button();
+                            var Line = new BoxView();
+                            int EmailId = (int)reader["ProfileEmailId"];
+
+                            emailProfile.Text = (string)reader["Name"];
+                            emailProfile.FontSize = 15;
+
+                            emailAddress.Text = (string)reader["Email"];
+                            emailAddress.FontSize = 25;
+                            emailAddress.HorizontalTextAlignment = TextAlignment.Center;
+
+                            deleteProfile.Text = "B";
+                            deleteProfile.TextColor = Color.Black;
+                            deleteProfile.FontSize = 10;
+                            deleteProfile.BackgroundColor = Color.FromHex("#f9a589");
+                            deleteProfile.CornerRadius = 15;
+                            deleteProfile.HeightRequest = 30;
+                            deleteProfile.WidthRequest = 30;
+                            deleteProfile.HorizontalOptions = LayoutOptions.End;
+                            deleteProfile.Clicked += new EventHandler((sender, e) => DeleteBoxEmail(sender, e, BoxId, EmailId));
+
+                            Line.HeightRequest = 1;
+                            Line.Color = Color.FromHex("#FF5521");
+
+                            ProfilesList.Children.Add(emailProfile);
+                            ProfilesList.Children.Add(emailAddress);
+                            ProfilesList.Children.Add(deleteProfile);
+                            ProfilesList.Children.Add(Line);
+                        }
+                    }
+                    connection.Close();
+                }
+            }
+
+
             //Marcar o desmarcar la box predeterminada
             void CheckDefaultBox(object sender, EventArgs e)
             {
@@ -175,14 +296,73 @@ namespace Mynfo.Views
             await Navigation.PushAsync(new ProfilesBYPESMPage());
         }
 
-        private async void UpdateBoxName(object sender, EventArgs e, int _BoxId, string _name, int _UserId)
+        private void UpdateBoxName(object sender, EventArgs e, int _BoxId, string _name, int _UserId)
         {
             //Actualizar el nombre de la Box
             string queryUpdateBoxName = "update dbo.Boxes set Name = '" + _name + "' where dbo.Boxes.UserId = " + _UserId + " and dbo.Boxes.BoxId = " + _BoxId;
             string cadenaConexion = @"data source=serverappmyinfonfc.database.windows.net;initial catalog=mynfo;user id=adminatxnfc;password=4dmiNFC*Atx2020;Connect Timeout=60";
             StringBuilder sb;
 
+            using (SqlConnection connection = new SqlConnection(cadenaConexion))
+            {
+                sb = new System.Text.StringBuilder();
+                sb.Append(queryUpdateBoxName);
+                string sql = sb.ToString();
 
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
         }
+        private void DeleteBoxPhone(object sender, EventArgs e, int _BoxId, int _PhoneId)
+        {
+            //Borrar la relación de la box con el teléfono
+            string queryUpdateBoxName = "delete from dbo.Box_ProfilePhone where dbo.Box_ProfilePhone.BoxId = " + _BoxId + " and dbo.Box_ProfilePhone.ProfilePhoneId = " + _PhoneId;
+            string cadenaConexion = @"data source=serverappmyinfonfc.database.windows.net;initial catalog=mynfo;user id=adminatxnfc;password=4dmiNFC*Atx2020;Connect Timeout=60";
+            StringBuilder sb;
+
+            using (SqlConnection connection = new SqlConnection(cadenaConexion))
+            {
+                sb = new System.Text.StringBuilder();
+                sb.Append(queryUpdateBoxName);
+                string sql = sb.ToString();
+
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+            Application.Current.MainPage = new NavigationPage(new DetailsBoxPage(_BoxId));
+        }
+
+        private void DeleteBoxEmail(object sender, EventArgs e, int _BoxId, int _EmailId)
+        {
+            //Borrar la relación de la box con el correo
+            string queryUpdateBoxName = "delete from dbo.Box_ProfileEmail where dbo.Box_ProfileEmail.BoxId = " + _BoxId + " and dbo.Box_ProfileEmail.ProfileEmailId = " + _EmailId;
+            string cadenaConexion = @"data source=serverappmyinfonfc.database.windows.net;initial catalog=mynfo;user id=adminatxnfc;password=4dmiNFC*Atx2020;Connect Timeout=60";
+            StringBuilder sb;
+
+            using (SqlConnection connection = new SqlConnection(cadenaConexion))
+            {
+                sb = new System.Text.StringBuilder();
+                sb.Append(queryUpdateBoxName);
+                string sql = sb.ToString();
+
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+            Application.Current.MainPage = new NavigationPage(new DetailsBoxPage(_BoxId));
+        }
+
+
     }
 }
