c5a4cbe28a7864937572172cab92b35810003af8
Cynthia Karina <40874570+CynthiaKarina@users.noreply.github.com>
2020-10-26 14:55:08 -0600

    Colores

--- Mynfo/ViewModels/BoxRegisterViewModel.cs
+++ Mynfo/ViewModels/BoxRegisterViewModel.cs
@@ -59,104 +59,104 @@
         }
         private async void SaveBox()
         {
-            //if (string.IsNullOrEmpty(this.Name))
-            //{
-            //    await Application.Current.MainPage.DisplayAlert(
-            //        Languages.Error,
-            //        Languages.NameValidation,
-            //        Languages.Accept);
-            //    return;
-            //}
+            if (string.IsNullOrEmpty(this.Name))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    Languages.Error,
+                    Languages.NameValidation,
+                    Languages.Accept);
+                return;
+            }
 
-            //this.IsRunning = true;
-            //this.IsEnabled = false;
+            this.IsRunning = true;
+            this.IsEnabled = false;
 
-            //var checkConnetion = await this.apiService.CheckConnection();
-            //if (!checkConnetion.IsSuccess)
-            //{
-            //    this.IsRunning = false;
-            //    this.IsEnabled = true;
-            //    await Application.Current.MainPage.DisplayAlert(
-            //        Languages.Error,
-            //        checkConnetion.Message,
-            //        Languages.Accept);
-            //    return;
-            //}
+            var checkConnetion = await this.apiService.CheckConnection();
+            if (!checkConnetion.IsSuccess)
+            {
+                this.IsRunning = false;
+                this.IsEnabled = true;
+                await Application.Current.MainPage.DisplayAlert(
+                    Languages.Error,
+                    checkConnetion.Message,
+                    Languages.Accept);
+                return;
+            }
 
-            //var mainViewModel = MainViewModel.GetInstance();
-            //DateTime boxTime = DateTime.Now;
-            //System.Text.StringBuilder sb;
-            //bool defaultBoxExists = false;
-            //Box box;
-            //string cadenaConexion = @"data source=serverappmyinfonfc.database.windows.net;initial catalog=mynfo;user id=adminatxnfc;password=4dmiNFC*Atx2020;Connect Timeout=60";
-            //string QueryToFindDefaultboxes = "select * from dbo.Boxes where dbo.Boxes.BoxDefault = 1 and dbo.Boxes.UserId = " 
-            //                                    + mainViewModel.User.UserId;
-
-            //using (SqlConnection connection = new SqlConnection(cadenaConexion))
-            //{
-            //    sb = new System.Text.StringBuilder();
-            //    sb.Append(QueryToFindDefaultboxes);
-            //    string sql = sb.ToString();
-
-            //    using (SqlCommand command = new SqlCommand(sql, connection))
-            //    {
-            //        connection.Open();
-            //        using (SqlDataReader reader = command.ExecuteReader())
-            //        {
-            //            while (reader.Read())
-            //            {
-            //                defaultBoxExists = true;
-            //            }
-            //        }
-            //        connection.Close();
-            //    }
-            //}
-            //if(defaultBoxExists)
-            //{
-            //    box = new Box
-            //    {
-            //        Name = this.Name,
-            //        BoxDefault = false,
-            //        UserId = mainViewModel.User.UserId,
-            //        Time = boxTime,
-            //    };
-            //}
-            //else
-            //{
-            //    box = new Box
-            //    {
-            //        Name = this.Name,
-            //        BoxDefault = true,
-            //        UserId = mainViewModel.User.UserId,
-            //        Time = boxTime,
-            //    };
-            //}
-
-
-
-            //var apiSecurity = Application.Current.Resources["APISecurity"].ToString();
-            //var response = await this.apiService.Post(
-            //    apiSecurity,
-            //    "/api",
-            //    "/Boxes",
-            //    box);
-
-            //if (!response.IsSuccess)
-            //{
-            //    this.IsRunning = false;
-            //    this.IsEnabled = true;
-            //    await Application.Current.MainPage.DisplayAlert(
-            //        Languages.Error,
-            //        response.Message,
-            //        Languages.Accept);
-            //    return;
-            //}
-
-            //this.IsRunning = false;
-            //this.IsEnabled = true;
-
-            //this.Name = string.Empty;
             var mainViewModel = MainViewModel.GetInstance();
+            DateTime boxTime = DateTime.Now;
+            System.Text.StringBuilder sb;
+            bool defaultBoxExists = false;
+            Box box;
+            string cadenaConexion = @"data source=serverappmyinfonfc.database.windows.net;initial catalog=mynfo;user id=adminatxnfc;password=4dmiNFC*Atx2020;Connect Timeout=60";
+            string QueryToFindDefaultboxes = "select * from dbo.Boxes where dbo.Boxes.BoxDefault = 1 and dbo.Boxes.UserId = "
+                                                + mainViewModel.User.UserId;
+
+            using (SqlConnection connection = new SqlConnection(cadenaConexion))
+            {
+                sb = new System.Text.StringBuilder();
+                sb.Append(QueryToFindDefaultboxes);
+                string sql = sb.ToString();
+
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            defaultBoxExists = true;
+                        }
+                    }
+                    connection.Close();
+                }
+            }
+            if (defaultBoxExists)
+            {
+                box = new Box
+                {
+                    Name = this.Name,
+                    BoxDefault = false,
+                    UserId = mainViewModel.User.UserId,
+                    Time = boxTime,
+                };
+            }
+            else
+            {
+                box = new Box
+                {
+                    Name = this.Name,
+                    BoxDefault = true,
+                    UserId = mainViewModel.User.UserId,
+                    Time = boxTime,
+                };
+            }
+
+
+
+            var apiSecurity = Application.Current.Resources["APISecurity"].ToString();
+            var response = await this.apiService.Post(
+                apiSecurity,
+                "/api",
+                "/Boxes",
+                box);
+
+            if (!response.IsSuccess)
+            {
+                this.IsRunning = false;
+                this.IsEnabled = true;
+                await Application.Current.MainPage.DisplayAlert(
+                    Languages.Error,
+                    response.Message,
+                    Languages.Accept);
+                return;
+            }
+
+            this.IsRunning = false;
+            this.IsEnabled = true;
+
+            this.Name = string.Empty;
+            //var mainViewModel = MainViewModel.GetInstance();
             mainViewModel.DetailsBox = new DetailsBoxViewModel();
             Application.Current.MainPage = new NavigationPage(new DetailsBoxPage());
         }
