852962b9496b45508424bd1dcf607997841df4a8
Gerardo López Daza <glopez@atx.mx>
2020-10-31 07:53:44 -0600

    Modelado NFC y obtencion de datos

---
+++ Mynfo/Models/Get_nfc.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Mynfo.Models
+{
+    public class Get_nfc
+    {
+        [JsonProperty("nombre")]
+        public string Nombre { get; set; }
+
+        [JsonProperty("apellido_paterno")]
+        public string Apellido_paterno { get; set; }
+
+        [JsonProperty("apellido_materno")]
+        public string Apellido_materno { get; set; }
+
+        [JsonProperty("id_usuario")]
+        public string Id_usuario { get; set; }
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("url")]
+        public string Url { get; set; }
+    }                 
+}
