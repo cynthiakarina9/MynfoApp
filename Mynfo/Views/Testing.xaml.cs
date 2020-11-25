c2770acf8db9c5b7a1fdad6679318600f783cccd
Rodrigo Rodriguez Toachee <rrodriguez@atx.mx>
2020-11-24 20:08:50 -0600

    Cambios Roy

--- Mynfo/Views/Testing.xaml.cs
+++ Mynfo/Views/Testing.xaml.cs
@@ -1,4 +1,8 @@
-﻿using Rg.Plugins.Popup.Services;
+﻿using Android.Content;
+using Android.Content.PM;
+using Android.Support.V4.Content;
+using Android.Telephony;
+using Rg.Plugins.Popup.Services;
 using System;
 
 using Xamarin.Forms;
@@ -15,7 +19,20 @@ namespace Mynfo.Views
 
             AbrirPopUp.Clicked += new EventHandler((sender, e) => OpenPopupTest(sender,e));
 
-            BackButton.Clicked += new EventHandler((sender, e) => GoToHome());
+            //BackButton.Clicked += new EventHandler((sender, e) => GoToHome());
+
+            TelephonyManager mTelephonyMgr;
+
+            /*var permission = ContextCompat.CheckSelfPermission(Android.App.Application.Context, "READ_PHONE_STATE");
+
+            if(permission == Permission.Denied)
+            {
+
+            }*/
+
+            mTelephonyMgr = Android.App.Application.Context.GetSystemService(Context.TelephonyService) as TelephonyManager;
+
+            var Number = mTelephonyMgr.Line1Number;
         }
 
         private void GoToHome()
