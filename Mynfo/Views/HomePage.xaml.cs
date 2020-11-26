73a4065d80f97d5020cab97fad4f998fd962a05f
Gerardo López Daza <glopez@atx.mx>
2020-11-26 15:57:42 -0600

    Mejoras en el envio de Mensajes

--- Mynfo/Views/HomePage.xaml.cs
+++ Mynfo/Views/HomePage.xaml.cs
@@ -153,9 +153,6 @@
                 }
             }
 
-            //Llenar variable global
-            get_box();
-
             //Box 2
             if (boxes[0] != null)
             {
@@ -491,89 +488,7 @@
                 CreateBoxBtn.IsVisible = true;
                 CreateBoxBtn.IsEnabled = true;
             }            
-        }
-
-        public void get_box()
-        {
-            string json = "";
-            try
-            {
-                var Profile = new ProfileLocal();
-                var Profile_1 = new ProfileLocal();
-                var Box_Local = new BoxLocal();
-                using (var conn = new SQLite.SQLiteConnection(App.root_db))
-                {
-                    Profile_1 = conn.Table<ProfileLocal>().FirstOrDefault();
-                    Box_Local = conn.Table<BoxLocal>().FirstOrDefault();
-                    int coun = conn.Table<ProfileLocal>().Count();
-                    string json_header = null;
-                    string json_body = null;
-                    string json_value = null;
-
-                    json_header = "Box recibida correctamente!\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" +
-
-                           "¡";                    
-                    json_value = "{"
-                              + @"""BoxId"":""" + Box_Local.BoxId + @""",
-                                ""Name"":""" + Box_Local.Name + @""",
-                                ""BoxDefault"":""" + Box_Local.BoxDefault + @""",
-                                ""UserId"":""" + Box_Local.UserId + @""",
-                                ""Time"":""" + Box_Local.Time + @""",
-                                ""ImagePath"":""" + Box_Local.ImagePath + @""",
-                                ""UserTypeId"":""" + Box_Local.UserTypeId + @""",
-                                ""FirstName"":""" + Box_Local.FirstName + @""",
-                                ""LastName"":""" + Box_Local.LastName + @""",
-                                ""ImageFullPath"":""" + Box_Local.ImageFullPath + @""",
-                                ""FullName"":""" + Box_Local.FullName + @""",
-                                ""ProfileLocalId"":""" + Profile_1.ProfileLocalId + @""",
-                                ""IdBox"":""" + Profile_1.IdBox + @""",
-                                ""UserId_p"":""" + Profile_1.UserId + @""",
-                                ""ProfileName"":""" + Profile_1.ProfileName + @""",
-                                ""value"":""" + Profile_1.value + @""",
-                                ""ProfileType"":""" + Profile_1.ProfileType + @"""                                                              
-                                }";
-
-                    if (coun > 1)
-                    {
-                        for (int i = 1; i < coun; i++)
-                        {
-                            Profile = conn.Table<ProfileLocal>().ElementAt(i);
-
-                            json_body = "{"
-                              + @"""BoxId"":""" + Box_Local.BoxId + @""",
-                                ""Name"":""" + Box_Local.Name + @""",
-                                ""BoxDefault"":""" + Box_Local.BoxDefault + @""",
-                                ""UserId"":""" + Box_Local.UserId + @""",
-                                ""Time"":""" + Box_Local.Time + @""",
-                                ""ImagePath"":""" + Box_Local.ImagePath + @""",
-                                ""UserTypeId"":""" + Box_Local.UserTypeId + @""",
-                                ""FirstName"":""" + Box_Local.FirstName + @""",
-                                ""LastName"":""" + Box_Local.LastName + @""",
-                                ""ImageFullPath"":""" + Box_Local.ImageFullPath + @""",
-                                ""FullName"":""" + Box_Local.FullName + @""",
-                                ""ProfileLocalId"":""" + Profile.ProfileLocalId + @""",
-                                ""IdBox"":""" + Profile.IdBox + @""",
-                                ""UserId_p"":""" + Profile.UserId + @""",
-                                ""ProfileName"":""" + Profile.ProfileName + @""",
-                                ""value"":""" + Profile.value + @""",
-                                ""ProfileType"":""" + Profile.ProfileType + @"""                                                              
-                                }";
-
-                            json_value = json_value + ",\n" + json_body;
-                        }
-                        json_value = "[" + json_value + "]";
-                    }
-                    json = json_value;
-                }
-            }
-            catch (Exception exx)
-            {
-                Console.Write(exx);
-                json = null;
-            }
-            Data_ntc.data_value = null;
-            Data_ntc.data_value = json;
-        }        
+        }       
 
         //Ir hacia detalles de la box
         private void BoxDetailsView(object sender, EventArgs e, int _BoxId)
@@ -819,7 +734,7 @@
 
                     if(valBoxLocal == true && valProfileLocal == true)
                     {
-                        this.get_box();
+                        //this.get_box();
                     }
                 }
                 else
@@ -1004,7 +919,7 @@
 
                         if (valBoxLocal == true && valProfileLocal == true)
                         {
-                            this.get_box();
+                            //this.get_box();
                         }
                     }
                 }
