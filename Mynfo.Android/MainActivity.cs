43e049054488c1c747ec96eb37fb5881442fb7a3
Rodrigo Rodriguez Toachee <rrodriguez@atx.mx>
2020-11-30 14:54:02 -0600

    l

--- Mynfo.Android/MainActivity.cs
+++ Mynfo.Android/MainActivity.cs
@@ -318,6 +318,12 @@
             ForeingBox      foreingBox;
             ForeingProfile  foreingProfile;
 
+            //Validar que la box no exista
+           /* using(var connSQLite = new SQLite.SQLiteConnection(App.root_db))
+            {
+                connSQLite.FindWithQuery<ForeingBox>("")
+            }*/
+
             //Inicializar la box foranea
             foreingBox = new ForeingBox
             {
