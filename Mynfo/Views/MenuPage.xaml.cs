12ca8519dd91b5a883691610c5f09326c6f3d9f8
Gerardo López Daza <glopez@atx.mx>
2020-12-03 12:02:59 -0600

    update

--- Mynfo/Views/MenuPage.xaml.cs
+++ Mynfo/Views/MenuPage.xaml.cs
@@ -27,39 +27,3 @@ namespace Mynfo.Views
         }        
     }
 }
-
-public class DetectShakeTest
-{
-    // Set speed delay for monitoring changes.
-    Xamarin.Essentials.SensorSpeed speed = Xamarin.Essentials.SensorSpeed.Game;
-
-    public DetectShakeTest()
-    {
-        // Register for reading changes, be sure to unsubscribe when finished
-        Xamarin.Essentials.Accelerometer.ShakeDetected += Accelerometer_ShakeDetected;
-    }
-
-    void Accelerometer_ShakeDetected(object sender, EventArgs e)
-    {
-        // Process shake event
-    }
-
-    public void ToggleAccelerometer()
-    {
-        try
-        {
-            if (Xamarin.Essentials.Accelerometer.IsMonitoring)
-                Xamarin.Essentials.Accelerometer.Stop();
-            else
-                Xamarin.Essentials.Accelerometer.Start(speed);
-        }
-        catch (Xamarin.Essentials.FeatureNotSupportedException fnsEx)
-        {
-            // Feature not supported on device
-        }
-        catch (Exception ex)
-        {
-            // Other error has occurred.
-        }
-    }
-}
