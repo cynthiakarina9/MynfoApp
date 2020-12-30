ee266513cc1ddd2d3a81500a5e879ee707d6cac4
Gerardo Daza López <glopez@atx.mx>
2020-12-30 10:24:40 -0600

    lectura NFC dispositivos y tag

--- Mynfo/Views/ListForeignBoxPage.xaml.cs
+++ Mynfo/Views/ListForeignBoxPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿namespace Mynfo.Views
 {
     using Mynfo.Models;
+    using Mynfo.Services;
     using Mynfo.ViewModels;
     using System;
     using System.Collections.Generic;
@@ -12,16 +13,55 @@
     {
         #region Contructor
 
-        
-        public ListForeignBoxPage()
+        #region Services
+        ApiService apiService;
+        #endregion
+
+        #region Properties
+        public IList<ForeingBox> foreingBox { private set; get; }
+        #endregion
+
+        public ListForeignBoxPage(int _ForeignUserId = 0)
         {           
             InitializeComponent();
 
+            apiService = new ApiService();
 
-
-           
+            GetUSer(_ForeignUserId);
+            GetList();
 
             BindingContext = this;
+        }      
+
+        public async void GetUSer(int _ForeignUserId)
+        {
+            var apiSecurity = Application.Current.Resources["APISecurity"].ToString();
+            if (_ForeignUserId != 0)
+            {
+                var response = await this.apiService.GetUserId(
+                apiSecurity,
+                "/api",
+                "/Users",
+                _ForeignUserId);
+            }
+        }
+
+        public void GetList()
+        {
+            List<ForeingBox> foreignBoxList = new List<ForeingBox>();
+            foreingBox = new List<ForeingBox>();
+
+            using (var conn = new SQLite.SQLiteConnection(App.root_db))
+            {
+                int a = conn.Table<ForeingProfile>().Count();
+
+                foreignBoxList = conn.Table<ForeingBox>().ToList();
+            }
+
+            foreach (ForeingBox foreingBoxValue in foreignBoxList)
+            {
+                foreingBox.Add(foreingBoxValue);
+            }
         }
         #endregion
 
