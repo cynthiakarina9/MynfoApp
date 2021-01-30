788b1dfd723d8b461abc73bb39c75e56e72ff40a
ccuesta <ccuesta@TI-CK>
2021-01-29 18:30:51 -0600

    Cambios Imagenes

--- Mynfo/ViewModels/TAGViewModel.cs
+++ Mynfo/ViewModels/TAGViewModel.cs
@@ -3,14 +3,18 @@
     using GalaSoft.MvvmLight.Command;
     using Mynfo.Services;
     using Mynfo.Views;
+    using System;
     using System.Windows.Input;
+    using Xamarin.Essentials;
     using Xamarin.Forms;
+    using Helpers;
 
     public class TAGViewModel
     {
         #region Services
         ApiService apiService;
         #endregion
+
 
         #region Constructor
         public TAGViewModel()
@@ -32,6 +36,18 @@
         {
             MainViewModel.GetInstance().Home = new HomeViewModel();
             Application.Current.MainPage = new MasterPage();
+        }
+        public ICommand GoToStoreCommand
+        {
+            get
+            {
+                return new RelayCommand(GoToStore);
+            }
+        }
+
+        private async void GoToStore()
+        {
+            await Launcher.OpenAsync(new Uri("https://mynfo.mx/index.php/tienda/"));
         }
         #endregion
     }
--- Mynfo/Views/TAGPage.xaml.cs
+++ Mynfo/Views/TAGPage.xaml.cs
@@ -6,12 +6,15 @@
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class TAGPage : ContentPage
     {
+        #region Attributes
+        private Label uno = new Label();
+        #endregion
         public TAGPage()
         {
             InitializeComponent();
-            Label uno = new Label();
             uno.Text = Languages.ConfigureTAG;
             uno.TextColor = Color.FromHex("#FF5521");
+            uno.FontSize = 22;
             Press.Text = Languages.Push + " '" + uno.Text + "' " + Languages.AndStick;
         }
     }
