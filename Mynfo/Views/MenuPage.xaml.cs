2a0e72017ae11eb5f1f70df35d165eb442017c6d
Gerardo Daza López <glopez@atx.mx>
2021-02-22 18:05:21 -0600

    update

--- Mynfo/Views/MenuPage.xaml.cs
+++ Mynfo/Views/MenuPage.xaml.cs
@@ -23,12 +23,16 @@
             MenuItemViewModel tappedItem = e.Item as MenuItemViewModel;
         }
         void OnToggled(object sender, ToggledEventArgs e)
-        {
+        {                      
             try
             {
+                bool value = e.Value;
+                int share = 1;
+                if (value == true) { share = 1; }
+                if (value == false) { share = 0; }
                 int user_id = MainViewModel.GetInstance().User.UserId;
                 string cadenaConexion = @"data source=serverappmynfo1.database.windows.net;initial catalog=mynfo;user id=adminmynfo;password=4dmiNFC*Atx2020;Connect Timeout=60";
-                string queryLastBoxCreated = @"UPDATE Users SET Share = 0 where UserId ="+ user_id;                
+                string queryLastBoxCreated = @"UPDATE Users SET Share = "+ share + "where UserId ="+ user_id;                
                 
                 SqlConnection con = new SqlConnection(cadenaConexion);
                 SqlCommand sqlcom = new SqlCommand(queryLastBoxCreated, con);
